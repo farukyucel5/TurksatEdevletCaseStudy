ca25621f80ff6f51a1b7d407d237258679d92881
farukyucel5 <farukyucel5@gmail.com>
2023-05-29 22:48:24 +0300

    update

--- src/test/java/hooks/api/HooksAPI.java
+++ src/test/java/hooks/api/HooksAPI.java
@@ -1,21 +1,6 @@
 package hooks.api;
 
-import io.cucumber.java.Before;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.RequestSpecification;
-import utilities.ConfigReader;
-
 public class HooksAPI {
-
-    public static RequestSpecification spec;
-
-    @Before (order = 0)
-    public void setUpApi(){
-        spec = new RequestSpecBuilder().setBaseUri(ConfigReader.getProperty("base_url")).build();
-    }
-
-
-
 
 
 }
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,54 @@
+package utilities;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private Driver(){
+
+    }
+
+    static WebDriver driver;
+
+    public static WebDriver getDriver(){
+
+        String browser= ConfigReader.getProperty("browser");
+        if(driver==null) {
+
+            switch (browser) {
+                case "firefox" -> driver = new FirefoxDriver();
+                case "safari" -> driver = new SafariDriver();
+                default -> driver = new ChromeDriver();
+            }
+
+
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }
+
+        return driver;
+
+    }
+
+    public static void closeDriver(){
+
+        if (driver != null){
+            driver.close();
+            driver=null;
+        }
+
+    }
+
+    public static void quitDriver(){
+        if (driver != null){
+            driver.quit();
+            driver=null;
+        }
+    }
+}
